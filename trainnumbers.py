8b8cb0b719407f905c5adf044ba51fd856b7406f
Ronald Chiang <rschaing@gmail.com>
2022-05-16 22:57:35 +1000

    simplifying loops

--- trainnumbers.py
+++ trainnumbers.py
@@ -1,14 +1,14 @@
+from itertools import product
+
 def finder(operations, no, formats):
-    for h in formats:
-        for i in operations:
-            for j in operations:
-                for k in operations:
-                    try:
-                        equation = h[0] + no[0] + h[1] + i + h[2] + no[1] + h[3] + j + h[4] + no[2] + h[5] + k + h[6] + no[3] + h[7]
-                        if eval(equation) == 10:
-                            print(equation)
-                    except:
-                        pass
+    for h, i, j, k in product(formats,operations,operations,operations):
+        try:
+            equation = h[0] + no[0] + h[1] + i + h[2] + no[1] + h[3] + j + h[4] + no[2] + h[5] + k + h[6] + no[3] + h[7]
+            if eval(equation) == 10:
+                print(equation)
+        except:
+            pass
+
 def main():
     operations = ["+","-","*","/"]
     formats = [
