554bc6ac27b83b5f52d33f43a15fbca746d7f7e2
Ronald Chiang <rschaing@gmail.com>
2022-05-16 12:05:05 +1000

    upload

---
+++ README.md
@@ -0,0 +1,9 @@
+Basic program to find the solutions to the train number game.
+
+Syndey Trains generally have a 4 digit number for each carriage.
+A common game is to use basic arithmetic and each digit to make 10.
+eg. A solution to 5215 is 5+(2-1)*5
+
+trainnumbers.py doesn't change the order of the digits
+
+permedtn.py uses all permutations
---
+++ trainnumbers.py
@@ -0,0 +1,25 @@
+def finder(operations, no, formats):
+    for h in formats:
+        for i in operations:
+            for j in operations:
+                for k in operations:
+                    try:
+                        equation = h[0] + no[0] + h[1] + i + h[2] + no[1] + h[3] + j + h[4] + no[2] + h[5] + k + h[6] + no[3] + h[7]
+                        if eval(equation) == 10:
+                            print(equation)
+                    except:
+                        pass
+def main():
+    operations = ["+","-","*","/"]
+    formats = [
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "(", "", "", ")", "", ""],
+        ["(", "", "", ")", "(", "", "", ")"],
+        ["", "", "", "", "(", "", "", ")"],
+        ["(", "", "", ")", "", "", "", ""]
+    ]
+    no = input("What is your train number? ")
+    finder(operations, no, formats)
+
+if __name__ == "__main__":
+    main()
